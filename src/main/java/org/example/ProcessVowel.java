9c96ac154f167b4bb47a0d351ae87b93aa7012a6
bertoSC <A23AlbertoGC@sanclemente.local>
2024-10-17 14:09:11 +0200

    completado el vowel

--- src/main/java/org/example/ProcessVowel.java
+++ src/main/java/org/example/ProcessVowel.java
@@ -1,23 +1,36 @@
 package org.example;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 public class ProcessVowel {
 
     public static void main(String[] args) throws FileNotFoundException {
         char[] v = new char[]{'a', 'e', 'i', 'o', 'u'};
+        int suma = 0;
+        String temp;
         for (char c : v) {
-
             String command = "java -cp target\\classes org.example.ProcessBuilderVowels " + String.valueOf(c);
             ProcessBuilder pb = new ProcessBuilder(command.split("\\s"));
             pb.inheritIO();
+
             try {
                 pb.start();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-
         }
+
+        for (char c : v) {
+            try (var in = new BufferedReader(new FileReader(c + "_archivo.txt"))) {
+                while ((temp = in.readLine()) != null) {
+                    System.out.println("El número de :"+c+" es : " +temp);
+                    suma+=Integer.parseInt(temp);
+                }
+            } catch (IOException e) {
+                System.out.println("Error");
+            }
+        }
+        System.out.println(suma);
+
     }
 }
